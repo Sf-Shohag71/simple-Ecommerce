8ef98b06dc30cd9cff6e62a88bea9bf3e2d75fe5
Sf-Shohag71 <farid.192002038@green.ac.bd>
2023-04-05 01:56:19 +0600

    Completed orders page design and add review item design

---
+++ src/Loaders/cartProductsLoader.js
@@ -0,0 +1,22 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
+const cartProductsLoader = async() => {
+    const loadedProduct = await fetch('products.json')
+    const products = await loadedProduct.json()
+    //load cart data from localStorage
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+
+    console.log(storedCart);
+    for(const id in storedCart){
+        const addedProduct = products.find(product => product.id === id);
+        if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+
+    return savedCart;
+}
+export default cartProductsLoader;
---
+++ src/components/ReviewItem/ReviewItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import './ReviewItem.css'
+
+const ReviewItem = ({product}) => {
+    const {id, name, img, price, quantity} = product;
+    return (
+        <div className='review-item'>
+            <img src={img} alt="" />
+            <div className="review-details">
+                <p className='title'>{name}</p>
+                <p>Price: <span className='gold-text'>${price}</span></p>
+                <p>Quantity: <span className='gold-text'>{quantity}</span></p>
+            </div>
+            <button className='btn-delete'><FontAwesomeIcon className='delete-icon' icon={faTrashAlt} /></button>
+        </div>
+    );
+};
+
+export default ReviewItem;
